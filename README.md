27bc634f52b9bd3f98aa32b8dad6b84aeeac4bc9
Yuliyan Yordanov <yuliyan.yordanov@tesco.com>
2018-10-17 09:15:26 +0100

    0.1 - initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Content Blocker Detection(cbd) script - `js + php + GA Measurement Protocol`
+This script is a bait for content blockers to hide some elements or block a network request in order to reveil themselves. It will displatch and event to  the server so you can measure the impact of visits on your website with content blockers so you can estimate how much of your traffic is missing in your client-side Analytics suite.
+It should be a two-part script:
+* This part is the client one - for detection
+* The other part is the server one - a file or path to handle the call to Analytics
+
+## The client-side script (`cbd.js`)
+* Has to run after the DOM has loaded so that content blockers have started to scrape the page
+* Creates two DOM elements : an image and a paragraph
+* Targets both EasyList and EasyPrivacy lists that are used in all browser extensions content blockers
+* Firefox doesn't need a content blocker since `version 65` as it automatically blocks trackers
+* Makes an XMLHTTP request to your desired server path to send the analytics call
+* Uses `sessionStorage` to avoid making another call to Analytics within the same session. You can use cookies or any other method to achieve that. Or you may want to dispatch the event on every pageview - it is up to you.
+
+## The server
+* Use a random name for the server file that you will call from the client so it won't get blocked
+* I use Google Analytics API to send the Analytics request from server therefore the following steps are relevant to Google Analytics only:
+     * Do a GET request to GA using the [Google Analytics Measurement Protocol](https://developers.google.com/analytics/devguides/collection/protocol/v1/devguide "Google Analytics Measurement Protocol"). Here is the end point url: `www.google-analytics.com`
+     * Example [url parameters](https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters "Google Analytics Measurement Protocol parameters reference") : `v=1&tid=UA-XXXXX-Y&cid=555&t=event&ec=ContnetBlockerDetection&el=VisitWithContentBlocker`
+     * Test your Analytics call with the [Hit builder](https://ga-dev-tools.appspot.com/hit-builder/ "Google Analytics Measurement Protocol hit builder") first
+
+## Example call to server
+* GET : `https://yourdomain.com/a234r23r23e.php?a=true` if content blocker is present or `https://yourdomain.com/234r23r23e.php?a=false` if not
+* POST : `https://yourdomain.com/a234r23r23e.php` with arguments = `a=true/false`
+
+## Simplified Example call from server with PHP
+* Assume you have a PHP file on the server called `a234r23r23e.php` with the following contents:
+     ```php
+     $sGaUrl = 'http://www.google-analytics.com/collect?v=1&tid=UA-XXXXX-Y&cid=555&hc=hostNameHere&dp=pathHere&dt=pageTitleHere&t=event&ec=ContnetBlockerDetection';
+     if(($_POST["a"] && $_POST["a"] == "true") || ($_GET["a"] && $_GET["a"] == "true") ){
+          file_get_contents($sGaUrl.'&el=VisitWithContentBlocker');
+     }else{
+          file_get_contents($sGaUrl.'&el=VisitWithoutContentBlocker');
+     }
+     ```
---
+++ cbd.js
@@ -0,0 +1,112 @@
+/**
+ * Created by YULIYAN on Apr/2018
+ * v 0.1
+ */
+/*jshint esversion:6*/
+/* globals window, document, console, clearTimeout, setTimeout  */
+"use strict";
+/*
+ * Detect and report if content blockers are in effect on client browser.
+ *
+ * Script to run after DOM has loaded, at the end of the page to minimize any performance impact (should be negligible).
+ */
+
+/**
+ * Using IIFE to avoid polluting the global scope.
+ * IE complaints about strict mode , therefore omiting it.
+ * @param {cbdPath}  [STRING____the_server_url_to_call]
+ */
+(function (cbdPath) {
+    if (!cbdPath) {
+        return;
+    }
+
+    /** Log function for debugging
+     * will log if session storage has '_qa' key
+     * @param {type} [STRING___info_warning_danger_success]
+     * @param {msg} [STRING___the_text_to_display_in_console_log_]
+     */
+    var _log = function (msg, type) {
+        if (window.sessionStorage && window.sessionStorage.getItem('_qa')) {
+            var cstyles = [
+                'background:' +
+                (type === 'info' ?
+                    'aqua' :
+                    type === 'danger' ?
+                    'red' :
+                    type === 'success' ? 'green' : 'yellow'),
+                'color:' + (type === 'warning' || type === 'info' ? '#333' : '#fff'),
+                'padding: 2px 8px'
+            ].join(';');
+            console.log('%c' + msg + '', cstyles);
+        }
+    };
+
+    if (
+        window.sessionStorage &&
+        !window.sessionStorage.getItem('_asdasdqweqwe')
+    ) {
+        var easyPrivacyListPresent = false,
+            easyListPresent = false,
+            bdy = document.body || document.documentElement,
+            timeout = 500, // in ms
+            testImage = document.createElement('img'), // to test for easyPrivacyList
+            testDiv = document.createElement('div'); // to test for easyList
+        testImage.src = '/analytics-js/someImage.png';
+        testImage.height = 1;
+        testImage.width = 1;
+        testImage.style =
+            'position : absolute;bottom:0;z-index : -999;visibility: hidden;display :inline-block;';
+        testDiv.style =
+            'position : absolute;bottom:0;z-index : -999;visibility: hidden;width:1px;height :1px;';
+        testDiv.innerHTML = '<p>&nbsp;</p>'; // IE fix
+        testDiv.id = 'ADSPACE02';
+        testDiv.setAttribute(
+            'class',
+            'ADBox AD_area NewsAds ProductAd V7-advert ad--MAIN'
+        );
+        bdy.appendChild(testImage);
+        bdy.appendChild(testDiv);
+
+        /*
+         * setTimeout method is needed since some content blockers fail to do their change
+         * before I send the request to the server for unknown reasons(slow computer/browser etc.)
+         * I have found it works as expected when I give them some time to scrape the DOM and do their blocking
+         */
+        window.setTimeout(function () {
+            _log('testImage.offsetHeight ' + testImage.offsetHeight, 'info');
+            _log('testDiv.offsetHeight ' + testDiv.offsetHeight, 'info');
+            if (testImage.offsetHeight === 0) {
+                easyPrivacyListPresent = true;
+            }
+            if (testDiv.offsetHeight === 0) {
+                easyListPresent = true;
+            }
+            _log('easyPrivacyList Enabled? ' + easyPrivacyListPresent, 'info');
+            _log('easyList Enabled? ' + easyListPresent, 'info');
+            // remove the created test elements
+            testImage.parentNode.removeChild(testImage); // IE fix
+            testDiv.parentNode.removeChild(testDiv); // IE fix
+
+            /** do ajax GET/POST
+      * use your own ajax method if you wish, I am using native XMLHttpRequest
+      * @param {cbdPath} [STRING___path_to_server_file_or_route_to_handle_the_GA_call_from_the_server]
+_     */
+            var url = cbdPath,
+                client = new XMLHttpRequest(),
+                method = 'POST',
+                args = 'el=' + (easyPrivacyListPresent || easyListPresent ? 'true' : 'false') +
+                        '&_csrf=' + document.body.getAttribute('data-csrf-token'); // if needed
+            if (method === 'GET') {
+              client.open(method, url + '?' + args, true);
+              client.send();
+            }else{
+                client.open(method, url, true);
+                client.setRequestHeader( 'Content-type', 'application/x-www-form-urlencoded' ); // if needed
+                client.send(args);
+            }
+
+            window.sessionStorage.setItem('_asdasdqweqwe', 'true');
+        }, timeout); // end timeout
+    }
+})(cbdPath);
